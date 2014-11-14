071f1fd6f4b957414759c98be46fb1fa25f4433c
Lucas Nicodemus <shank@shanked.me>
2014-11-14 15:22:15 -0700

    Added /teams/ and /team/ routes.

--- app.rb
+++ app.rb
@@ -2,35 +2,11 @@ require 'sinatra'
 require_relative 'Score'
 
 class Magi < Sinatra::Base
-  get '/:state/:division/?' do
-    return erb :error, :locals => {:error => "This page has been disabled, because it contained invalid projection data."}
-    unless params[:division] == 'all-service' || params[:division] == 'open'
-      return erb :error, :locals => {:error => "Invalid division specified. Must either be 'open' or 'all-service'."}
-    end
 
-    score_count = Score.where({:state => params[:state], :division => params[:division]}).count
-
-    unless score_count > 0
-      return erb :error, :locals => {:error => "That state is either invalid, or no teams from that state's #{params[:division]} division have competed in this round yet."}
-    end
-
-    scores = Score.where({:state => params[:state], :division => params[:division]}).sort(:total_score.desc)
-
-    plat_slots = (score_count * 0.3).round(0)
-    plat_slots_save = plat_slots
-    scores.each do |score|
-      if plat_slots > 0 
-        score.platinum = true
-        plat_slots -= 1
-      else
-        score.platinum = false
-      end
-      score.save
-    end
-    puts "Returning #{score_count}."
-    erb :state_platinum, :locals => {:plat_slots => plat_slots_save, :scores => scores, :teams => score_count, :division => params[:division], :state => params[:state]}
+  get '/rhs/?' do
+    redirect to('/teams/07-0152,07-0327,07-1260,07-1262,97-1964,07-0158,07-0639,07-1818')
   end
-
+  
   get '/:division/?' do
     unless params[:division] == 'all-service' || params[:division] == 'open'
       return erb :error, :locals => {:error => "Invalid division specified. Must either be 'open' or 'all-service'."}
@@ -50,5 +26,37 @@ class Magi < Sinatra::Base
 
   configure do
     MongoMapper.setup({'production' => {'uri' => ENV['MONGODB_URI']}}, 'production')
+  end
+
+  get '/team/:teamid/?' do
+    scores = Score.where({:team_id => params[:teamid]}).sort(:total_score.desc)
+
+    if scores.count == 0
+      return erb :error, :locals => {:error => "Invalid team ID specified. Team must be a fully qualified ID, e.g. 07-0152."}
+    end
+
+    erb :team, :locals => {:scores => scores, :division => scores.first.division, :state => scores.first.state}
+  end
+
+  get '/teams/:teamids/?' do
+
+    unless (params[:teamids].include?(','))
+      return erb :error, :locals => {:error => "Invalid team CSV specified. Separate TeamIDs by commas."}
+    end
+
+    teams = Array.new
+    params[:teamids].split(',').each do |team|
+      sc = Score.where({:team_id => team}).sort(:total_score.desc).first
+
+      unless sc == nil
+        teams.push(sc)
+      end
+    end
+
+    if teams.count == 0
+      return erb :error, :locals => {:error => "Invalid team IDs specified. Teams must be fully qualified, e.g. 07-0152,06-0238, etc."}
+    end
+
+    erb :teams, :locals => {:teams => teams}
   end
 end
