1876126d6f0b516ade38bb3b5947562ea63e8235
Lucas Nicodemus <shank@shanked.me>
2014-11-14 10:16:11 -0700

    Update crawling & import system

---
+++ Score.rb
@@ -0,0 +1,20 @@
+require 'mongo_mapper'
+MongoMapper.setup({'production' => {'uri' => ENV['MONGODB_URI']}}, 'production')
+
+
+class Score
+  include MongoMapper::Document
+
+  key :team_id, String, :unique => true
+  key :r1_score, Integer
+  key :r2_score, Integer
+  key :division, String
+  key :state, String
+  key :images, Integer
+  key :time, String
+  key :warnings, String
+
+  Score.ensure_index(:team_id)
+  Score.ensure_index(:state)
+  Score.ensure_index(:division)
+end
--- crawl.rb
+++ crawl.rb
@@ -1,16 +1,27 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+require 'mongo_mapper'
+require_relative 'Score'
+MongoMapper.setup({'production' => {'uri' => ENV['MONGODB_URI']}}, 'production')
 
 @document = Nokogiri::HTML(open("http://54.243.195.23/"))
 @nodeset = @document.css("tr.clickable") # @document.xpath("//tr")
 
 @nodeset.each do |row|
   puts "GETTING A TEAM SCORE"
-  row.children.each do |cr|
-    puts "#{cr.children[0].to_s}"
-    
-  end
+  team_score = {
+    :id => row.children[0].children[0], 
+    :division => row.children[1].children[0], 
+    :state => row.children[2].children[0],
+    :images => row.children[3].children[0],
+    :time => row.children[4].children[0],
+    :score => row.children[5].children[0],
+    :warnings => row.children[6].children[0]
+  }
+  puts "Team is #{team_score[:id]}. They're at #{team_score[:score]} in #{team_score[:state]}'s #{team_score[:division]} division."
+
 end
 
 # binding.pry
+
